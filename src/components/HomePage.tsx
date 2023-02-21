8f021d81173fa1fa69bed18cb294f456671848c5
icaro.teodoro84@icloud.com <icaro.teodoro84@icloud.com>
2023-02-21 14:03:29 -0300

    responsividade

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,7 +10,7 @@ export function HomePage(){
         <div className='h-screen bg-app bg-no-repeat bg-cover flex relative'>
             <title>Palmeira Bike Shop</title>
             <Image className='my-auto mx-auto sm:mx-auto md:mx-96 lg:mx-96 w-72 md:w-80 lg:w-96' src={logoHome} alt=""></Image>
-            <p className="absolute bottom-9 lg:bottom-14 left-24 sm:left-52 md:left-72 text-white text-2xl lg:text-6xl font-sunflower">SUA LOJA DE CICLISMO.</p>
+            <p className="absolute bottom-9 lg:bottom-12 left-24 sm:left-52 md:left-72 text-white text-2xl lg:text-6xl font-sunflower">SUA LOJA DE CICLISMO.</p>
         </div>
     );
 }
