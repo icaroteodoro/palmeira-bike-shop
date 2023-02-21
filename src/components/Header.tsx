59f896ae718f8c517de4c3dace6eb2f7368a57fe
icaro.teodoro84@icloud.com <icaro.teodoro84@icloud.com>
2023-02-21 14:01:10 -0300

    responsividade

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ import search from '../assets/search.svg';
 
 export function Header(){
     return (
+        // HEADER
         <div className='fixed w-full z-10'>
             <div className='bg-gray-700 mx-auto items-center h-8 flex'>
                 <div className='absolute right-0 flex text-white'>
@@ -17,21 +18,24 @@ export function Header(){
                 </div>
                 </div>
             </div>
+
             <div className='bg-white h-16 flex'>
-                <h1 className='font-bold text-2xl my-auto ml-32 font-K2D italic'>Palmeira Bike Shop</h1>
-                <div className='my-auto mx-auto bg-gray-200 h-10 w-4/12 rounded-lg relative'>
+                <h1 className='font-bold text-1xl md:text-2xl my-auto ml-10 lg:ml-32 font-K2D italic'>Palmeira Bike Shop</h1>
+                {/* PESQUISA */}
+                <div className='my-auto mx-auto bg-gray-200 h-10 w-4/12 rounded-lg relative hidden sm:flex'>
                     <input placeholder='O que você está procurando?' className='bg-transparent h-full w-full rounded static p-3 font-K2D italic text-lg' type="text" />
                     <button className='absolute h-full right-1'>
                         <Image className='my-auto w-7 mr-2' src={search} alt=""/>
                     </button>
                 </div> 
-                <div className='flex my-auto p-5 w-56'>
+                {/* PROFILE  FAV CAR */}
+                <div className='my-auto p-5 w-56 hidden sm:flex'>
                     <a href="" className='mx-auto'><Image src={profileIcon} alt="" /></a>
                     <a href="" className='mx-auto'><Image src={favIcon} alt="" /></a>
                     <a href="" className='mx-auto'><Image src={carIcon} alt="" /></a>
                 </div>
             </div>
-            <div className='h-9 bg-gray-200 text-2xl font-sunflower text-gray-400 flex'>
+            <div className='h-9 bg-gray-200 text-2xl font-sunflower text-gray-400 hidden md:flex'>
                 <div className='mx-auto my-auto'>   
                     <a href="" className='m-5'>Masculino</a>
                     <a href="" className='m-5'>Feminino</a>
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,8 +9,8 @@ export function HomePage(){
     return (
         <div className='h-screen bg-app bg-no-repeat bg-cover flex relative'>
             <title>Palmeira Bike Shop</title>
-            <Image className='my-auto mx-96' src={logoHome} alt=""></Image>
-            <p className="absolute bottom-14 left-72 text-white text-6xl font-sunflower">SUA LOJA DE CICLISMO.</p>
+            <Image className='my-auto mx-auto sm:mx-auto md:mx-96 lg:mx-96 w-72 md:w-80 lg:w-96' src={logoHome} alt=""></Image>
+            <p className="absolute bottom-9 lg:bottom-14 left-24 sm:left-52 md:left-72 text-white text-2xl lg:text-6xl font-sunflower">SUA LOJA DE CICLISMO.</p>
         </div>
     );
 }
